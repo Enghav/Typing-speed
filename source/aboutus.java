794a2270d8fcbf591a32471302f927d922d68528
Seng Enghav <128684916+Enghav@users.noreply.github.com>
2023-03-24 22:14:02 +0700

    Create aboutus.java

---
+++ source/aboutus.java
@@ -0,0 +1,28 @@
+package source;
+
+public class aboutus {
+    
+    public static void main(String[] args){
+        System.out.println(""" 
+    
+        This a program that use for typing speed test and we have 6 member who constritibuted to this project.
+        This program have: 
+        - Login page
+        - Register page
+        - Reset password
+        - User typing test
+        - ResultList
+        ------------------------------------------
+        Our collaborator are:
+            1. Register Page          = San Vannak
+            2. Landing/Reset Password = Sam Jefferson
+            3. User Login             = Soeun Sopanharak
+            4. ResultList             = Vuthy Chheangsrun
+            5. User Typing Test       = Seng Enghav
+            6. About Us Page          = Lim Techchhiv
+        ------------------------------------------
+        """ ); 
+    }
+
+    
+}
