88f6bfe917997058591ffe2297193faba6335b62
Enghav <128684916+Enghav@users.noreply.github.com>
2023-03-24 21:09:57 +0700

    Update main.java

--- source/main.java
+++ source/main.java
@@ -4,13 +4,26 @@ public class main {
     public static void main (String[] args) {
         Scanner input = new Scanner (System.in );
         System.out.println("Choose any option :");
-        System.out.println("1. User Regiter page: \n2.User Reset password : \n 3.User Login page : \n 4.Typing Page : \n 5.Result list : \n 6.About us ");
+        System.out.println(" 1.User Regiter page: \n 2.User Reset password : \n 3.User Login page : \n 4.Typing Page : \n 5.Result list : \n 6.About us ");
         System.out.println("Enter options : ");
         int option = input.nextInt();
         switch(option ){
-            case 1 : Register();
+            case 1 : Register.main(args);
             break;
-            case 2: ResetPw.
+            case 2: ResetPw.main(args);
+            break;
+            case 3: userlogin.main(args);
+            break;
+            case 4: usertyping.main(args);
+            break;
+            case 5 :ResultList.main(args);
+            break;
+            case 6: aboutus.main(args);
+            break;
+            case 0: 
+            System.out.println("Press any key to continue........");
+            System.exit(0); 
+            
         }
 
     }
