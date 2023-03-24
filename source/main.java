4c31c6c943ec38a20a57f8b70234390c69357f03
Seng Enghav <128684916+Enghav@users.noreply.github.com>
2023-03-24 20:24:41 +0700

    Create main.java

---
+++ source/main.java
@@ -0,0 +1,17 @@
+package source;
+import java.util.Scanner ;
+public class main {
+    public static void main (String[] args) {
+        Scanner input = new Scanner (System.in );
+        System.out.println("Choose any option :");
+        System.out.println("1. User Regiter page: \n2.User Reset password : \n 3.User Login page : \n 4.Typing Page : \n 5.Result list : \n 6.About us ");
+        System.out.println("Enter options : ");
+        int option = input.nextInt();
+        switch(option ){
+            case 1 : Register();
+            break;
+            case 2: ResetPw.
+        }
+
+    }
+}
