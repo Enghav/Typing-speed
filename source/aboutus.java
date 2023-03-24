ce8738b02410cf3a47c88459eaf1686359fb97fd
PangLyhour <128688193+PangLyhour@users.noreply.github.com>
2023-03-24 23:11:49 +0700

    Add files via upload

--- source/aboutus.java
+++ source/aboutus.java
@@ -1,28 +1,27 @@
-package source;
+package Test;
 
 public class aboutus {
+    public static void main(String[] args) {
+    System.out.println(""" 
     
-    public static void main(String[] args){
-        System.out.println(""" 
-    
-        This a program that use for typing speed test and we have 6 member who constritibuted to this project.
-        This program have: 
-        - Login page
-        - Register page
-        - Reset password
-        - User typing test
-        - ResultList
-        ------------------------------------------
-        Our collaborator are:
-            1. Register Page          = San Vannak
-            2. Landing/Reset Password = Sam Jefferson
-            3. User Login             = Soeun Sopanharak
-            4. ResultList             = Vuthy Chheangsrun
-            5. User Typing Test       = Seng Enghav
-            6. About Us Page          = Lim Techchhiv
-        ------------------------------------------
-        """ ); 
+    This a program that use for typing speed test and we have 7 member who contributed to this project.
+    This program have: 
+    - Login page
+    - Register page
+    - Reset password
+    - User typing test
+    - ResultList
+    - Read Menu
+    ------------------------------------------
+    Our collaborator are:
+        1. Register Page          = San Vannak
+        2. Landing/Reset Password = Sam Jefferson
+        3. User Login             = Soeun Sopanharak
+        4. ResultList             = Vuthy Chheangsrun
+        5. User Typing Test       = Seng Enghav
+        6. About Us Page          = Lim Techchhiv
+        7. Read Menu              = Pang Lyhour
+    ------------------------------------------
+    """ );
     }
-
-    
 }
