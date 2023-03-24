cf62944494b55b2414477a68330a6be0d3d47959
VuthyChheangsrun <128031514+VuthyChheangsrun@users.noreply.github.com>
2023-03-24 15:16:19 +0700

    fix

--- source/ResultList.java
+++ source/ResultList.java
@@ -1,7 +1,7 @@
 package source;
 
 public class ResultList {
-    void Display(){
+    public static void main(String[] args) {
         System.out.println(
             """
             Test Result:
--- source/usertyping.java
+++ source/usertyping.java
@@ -1,4 +1,4 @@
-package s2java;
+package source;
 import java.util.Scanner;
 public class usertyping {
     public static void main(String[] args) {
